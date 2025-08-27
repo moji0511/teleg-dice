42a7e7bb6a68249872f12bc141058da2f0bca5f2
moji0511 <moji.0511@gmail.com>
2025-08-27 22:01:53 +0330

    main.py

--- main.py
+++ main.py
@@ -7,14 +7,8 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['tas'])
 def send_dice(message):
-    # ارسال استیکر تاس
     bot.send_dice(message.chat.id, emoji='🎲')
-    
-    # تولید عدد تصادفی بین ۱ تا ۶
     dice_number = random.randint(1, 6)
-    
-    # ارسال پیام عدد تاس
     bot.send_message(message.chat.id, f"عدد تاس شما هست: {dice_number}")
 
-# اجرای ربات به صورت polling
 bot.infinity_polling()
